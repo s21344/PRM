89ef4adca969d3d68646a2c252d34709b4b9e35d
Jakub-Wilczak <jakub.alek.wilczak@gmail.com>
2024-03-25 17:45:43 +0100

    Zad 1

---
+++ untitled/src/Zad1.kt
@@ -0,0 +1,10 @@
+
+fun main(){
+    var intvalue:Int = 2
+
+    // var value1:Double = 2 (You can;t assing Int to Double)
+    //var value2_1:Long = intvalue  //Long is differnet Object than INT
+    var value2_2:Long = 2  // but you can assing normal 2
+    // var value3:Char = 2 (You can;t assing Int to Char)
+    // var value4:String = 2 (You can;t assing Int to String)
+    }
