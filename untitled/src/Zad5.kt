87ddd7795cd698dbdfa6b72970b6514352f54b1a
Jakub-Wilczak <jakub.alek.wilczak@gmail.com>
2024-03-29 18:29:42 +0100

    corrected Zad5 (mapOf( ) to instead of = and [] and HashMap

--- untitled/src/Zad5.kt
+++ untitled/src/Zad5.kt
@@ -1,21 +1,21 @@
 
 fun main(){
 
-    var productsMap = LinkedHashMap<String,Double>()
-    productsMap["G Fuel"]= 149.99
-    productsMap["Candy"]= 10.00
-    productsMap["Coke Coke Cola"]= 5.99
-    productsMap["Fantamentos"]= 100.00
-    productsMap["Fries"]= 4.00
-    productsMap["Ham"]= 9.00
-    productsMap["Cheese"]= 4.50
+    var productsMap = mapOf(
+        "G Fuel" to 149.99,
+        "Candy" to 10.00,
+        "Coke Coke Cola" to 5.99,
+        "Fantamentos" to 100.00,
+        "Fries" to 4.00,
+        "Ham" to 9.00,
+        "Cheese" to 4.50
+    )
+
 
     var iteratedProductsMap1 = productsMap.map { it.value to it.value*0.8}
     var iteratedProductsMap2 = productsMap.map { it.value *0.8}
 
-    println(productsMap.forEach { (product, price) ->
-        println("$product = $price")
-    })
+
 
 
 
